469900bc95d0cbe13bc1597e9548164572e533d4
maureenmuys <maureen.muys@gmail.com>
2023-07-29 08:07:20 +0200

    add Catogory model, repository, service changes to AdminController

---
+++ src/main/java/com/mmuys/cool/model/Category.java
@@ -0,0 +1,25 @@
+package com.mmuys.cool.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    @Column(name = "category_id" )
+    private int id;
+
+    private String name;
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/mmuys/cool/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.mmuys.cool.repository;
+
+import com.mmuys.cool.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+
+
+
+}
