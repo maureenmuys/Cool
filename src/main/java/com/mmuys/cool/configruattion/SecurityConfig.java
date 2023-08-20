4ffbdefaa000c98095e372356795407766b097f2
maureenmuys <maureen.muys@gmail.com>
2023-08-20 14:28:47 +0200

    Cleanup

--- src/main/java/com/mmuys/cool/configruattion/SecurityConfig.java
+++ src/main/java/com/mmuys/cool/configruattion/SecurityConfig.java
@@ -1,26 +1,20 @@
 package com.mmuys.cool.configruattion;
 
-import com.mmuys.cool.provider.CustomAuthenticationProvider;
+
 import com.mmuys.cool.service.CustomerUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 
 @Configuration
 @EnableWebSecurity
